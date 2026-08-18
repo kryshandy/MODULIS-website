c96a6358b5481b651eeed3f528e0e17f0cde0809
SKREAM <freddychoudja@gmail.com>
2026-08-18 16:51:31 +0100

    seo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'node:path'
 const root = process.cwd()
 
 export default defineConfig({
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
